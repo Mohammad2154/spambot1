4f8ccd350ff1ba5200558e45bbdb5ae01a74c6b4
Mohammad2154 <54350077+Mohammad2154@users.noreply.github.com>
2019-08-21 10:57:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const myid = ['605275012785635349'];
-const prefix = ['$'];
+const prefix = ['!'];
 
 client.on('ready', () => {
    console.log(`----------------`);
